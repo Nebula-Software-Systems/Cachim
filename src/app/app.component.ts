0b1252c610c1020c1a471d34ffa4a64d1fca5418
Rafael Câmara <rafaelcamarac@gmail.com>
2023-06-27 20:35:14 +0100

    Add polling option to define interval to retrieve cache data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { InMemoryCache } from './models/in-memory-cache';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  POLLING_DATA_LOCAL_STORAGE_KEY = 'polling';
   title = 'Nebula.InMemoryVisualizer';
   inMemoryCache: InMemoryCache = new InMemoryCache(new Map<string, string>());
 
@@ -19,5 +20,22 @@ export class AppComponent {
       .subscribe((result: InMemoryCache) => {
         this.inMemoryCache = new InMemoryCache(new Map(Object.entries(result)));
       });
+
+    this.initPollingOption();
+  }
+
+  onSelected(value: string): void {
+    localStorage.setItem(this.POLLING_DATA_LOCAL_STORAGE_KEY, value);
+  }
+
+  private initPollingOption() {
+    let pollingDuration = localStorage.getItem(
+      this.POLLING_DATA_LOCAL_STORAGE_KEY
+    );
+    if (pollingDuration) {
+      (<HTMLOptionElement>(
+        document.getElementById(`polling_${pollingDuration}`)
+      )).selected = true;
+    }
   }
 }
