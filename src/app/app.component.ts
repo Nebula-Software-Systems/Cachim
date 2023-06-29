67ced01899f04c3dfd2ead4019ccc93951392249
Rafael Câmara <rafaelcamarac@gmail.com>
2023-06-29 17:13:45 +0100

    Refactor app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,14 +20,8 @@ export class AppComponent {
 
   ngOnInit(): void {
     this.initPollingOption();
-
-    setInterval(() => {
-      this.inMemoryCacheService
-        .getInMemoryCache()
-        .subscribe((result: InMemoryCacheRepresentation[]) => {
-          this.inMemoryCache = result;
-        });
-    }, this.polling_time_in_seconds * this.SECONDS_TO_MILLISECONDS_CONVERSION);
+    this.initRequestToFillCacheTable();
+    this.periodicRetrievalOfCacheInformation();
   }
 
   onSelected(value: string): void {
@@ -40,14 +34,35 @@ export class AppComponent {
     ) as number | null;
 
     if (pollingDuration) {
+      //set the element in local storage as the selected for the polling duration
       (<HTMLOptionElement>(
         document.getElementById(`polling_${pollingDuration}`)
       )).selected = true;
       this.polling_time_in_seconds = pollingDuration;
     } else {
+      //use a default value for the polling
       (<HTMLOptionElement>(
         document.getElementById(`polling_${pollingDuration}`)
       )).selected = true;
     }
+  }
+
+  private initRequestToFillCacheTable(): void {
+    this.retriveCacheDataFromServer();
+  }
+
+  private periodicRetrievalOfCacheInformation() {
+    setInterval(
+      () => this.retriveCacheDataFromServer(),
+      this.polling_time_in_seconds * this.SECONDS_TO_MILLISECONDS_CONVERSION
+    );
+  }
+
+  private retriveCacheDataFromServer(): void {
+    this.inMemoryCacheService
+      .getInMemoryCache()
+      .subscribe((result: InMemoryCacheRepresentation[]) => {
+        this.inMemoryCache = result;
+      });
   }
 }
